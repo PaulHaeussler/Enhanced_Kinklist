e458b398c0171ad160c37ce2bb568931ed6a9c27
Paul <paulhaeussler@gmx.de>
2022-03-08 00:22:54 +0100

    ip change

--- enhanced_kinklist.py
+++ enhanced_kinklist.py
@@ -65,7 +65,7 @@ class Kinklist:
             response.status_code = 200
             return response
 
-        self.app.run(host='127.0.0.1', port=5000)
+        self.app.run(host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     k = Kinklist()
